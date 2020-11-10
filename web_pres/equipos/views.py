9f769e072764ecf8d947e04ff0c3d3be755a3c68
Michael1996aml <Michael1996aml@gmail.com>
2020-11-10 17:10:30 -0300

    Operativa la ventana de los equipos
    se crearon los card donde iran los equipos
    se dejo operativa la pagina de visualizacion de estos

--- web_pres/equipos/views.py
+++ web_pres/equipos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Equipo
 
 # Create your views here.
+
+def verEquipos(request):
+    equipos = Equipo.objects.all().filter(estado=True)
+    return render(request,"equipos.html",{"equipos":equipos})
--- web_pres/web_pres/urls.py
+++ web_pres/web_pres/urls.py
@@ -4,6 +4,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("core.urls"))
+    path('',include("core.urls")),
+    path('equipos/',include("equipos.urls"))
    
 ]
